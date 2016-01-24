de2cd61a8c918d46e956f9f80a45320e27bb3f6e
Devin Fowler <fowlerd11@gmail.com>
2016-01-24 14:14:11 -0500

    Update proj-animations.js

--- js/proj-animations.js
+++ js/proj-animations.js
@@ -36,7 +36,7 @@ function closeProjInfo() {
     viewer.children.namedItem('project-viewer-demo').style.opacity = 0;
     viewer.querySelector('.project-viewer-description').style.opacity = 0
 
-    document.documentElement.style.overflow = 'auto'; // firefox, chrome
+    document.documentElement.style.overflowY = 'auto'; // firefox, chrome
     document.body.scroll = "yes"; // ie only
 
     overlay.style.visibility = 'hidden';
